349b71f9f038dab965c906f9196ce661be5add0b
awasunyin <awasunyin@gmail.com>
2017-10-18 14:08:07 +0200

    unsupervised example

--- unsupervised/plot_kmeans_digits.py
+++ unsupervised/plot_kmeans_digits.py
@@ -1,3 +1,13 @@
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import scale
+
 """
 ===========================================================
 A demo of K-Means clustering on the handwritten digits data
@@ -27,19 +37,10 @@ silhouette   silhouette coefficient
 """
 print(__doc__)
 
-from time import time
-import numpy as np
-import matplotlib.pyplot as plt
-
-from sklearn import metrics
-from sklearn.cluster import KMeans
-from sklearn.datasets import load_digits
-from sklearn.decomposition import PCA
-from sklearn.preprocessing import scale
-
 np.random.seed(42)
 
 digits = load_digits()
+
 data = scale(digits.data)
 
 n_samples, n_features = data.shape
